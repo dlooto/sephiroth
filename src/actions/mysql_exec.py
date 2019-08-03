823fb905b8efaa3426c132da95957e4f040538da
healer.kx <healer.kx.yu@gmail.com>
2019-08-03 23:00:19 +0800

    try

--- src/actions/mysql_exec.py
+++ src/actions/mysql_exec.py
@@ -13,15 +13,19 @@ class MySQLExecAction(BaseAction):
     def execute(self, context):
         result = None
         db = Resource.find_resource('global', 'mysqlconnection')
-        with db.cursor() as cursor:
-            action_config = self.get_action_config()
-            sql = context.evaluate(action_config['sql'])
-            # Logger
-            
-            self.log(sql)
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            print(result)
-            
-            return_var = self.get_return_var_name()
-            context.set_context_var(return_var, result)
+        
+        try:
+            with db.cursor() as cursor:
+                action_config = self.get_action_config()
+                sql = context.evaluate(action_config['sql'])
+                # Logger
+                
+                self.log(sql)
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                print(result)
+                
+                return_var = self.get_return_var_name()
+                context.set_context_var(return_var, result)
+        except Exception as e:
+            print(e.with_traceback())
