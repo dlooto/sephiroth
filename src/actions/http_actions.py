96d2dbd47c8d448b873e1f5f7048105d4c24e8fb
runforever <c.chenchao.c@gmail.com>
2020-08-13 23:40:50 +0800

    北京自动站数据转发到新平台

--- src/actions/http_actions.py
+++ src/actions/http_actions.py
@@ -1,7 +1,8 @@
-
 from .base_action import *
 import requests
 import json
+
+from urllib.parse import urlparse
 
 
 @Actions.register("http_get")
@@ -10,12 +11,12 @@ class HttpGetAction(BaseAction):
     :param url || string || @required
     :param content_type || string || @optional
     """
-    
+
     def execute(self, context):
 
         action_config = self.get_action_config()
         url = context.evaluate(action_config['url'])
-        
+
         resp = requests.get(url)
         value = resp.text
         self.log(value)
@@ -23,7 +24,7 @@ class HttpGetAction(BaseAction):
 
         if 'content_type' in action_config and action_config['content_type'] == 'json':
             value = json.loads(value)
-        
+
         context.set_context_var(return_var, value)
 
 
@@ -31,6 +32,18 @@ class HttpGetAction(BaseAction):
 class HttpPostAction(BaseAction):
     """
     """
+    def send_data2newplatform(self, post_url, data):
+        if '/d/send' not in post_url:
+            return False
+
+        device_id = post_url.split('/')[-1]
+        api = 'http://platform.scarletsun.wang/api/v1/receive/device/data/%s/' % device_id
+        try:
+            resp = requests.post(api, data)
+            self.log(resp.json())
+        except:
+            pass
+        return True
 
     def execute(self, context):
         action_config = self.get_action_config()
@@ -38,19 +51,21 @@ class HttpPostAction(BaseAction):
         param0 = self.get_param_var_name()
 
         data = context.get_context_var(param0)
-        
+
         post_url = context.evaluate(action_config['url'])
         resp = requests.post(post_url, data=data)
         value = resp.text
         print(value)
         if 'content_type' in action_config and action_config['content_type'] == 'json':
             value = json.loads(value)
-        
+
         return_var = self.get_return_var_name()
         context.set_context_var(return_var, value)
         resp.connection.close()
-        
+
         self.log(resp.text)
+        self.send_data2newplatform(post_url, data)
+        return True
 
 
 @Actions.register("http_post_file")
@@ -58,9 +73,19 @@ class HttpPostFileAction(BaseAction):
     """
     """
 
+    def upload2newplatform(self, post_url, file):
+        parse_url = urlparse(post_url)
+        query_params = parse_url.query
+        api = 'http://platform.scarletsun.wang/api/v1/device/hpge/upload/?%s' % query_params
+        try:
+            resp = requests.post(api, files={'file': file})
+            self.log(resp.status_code)
+        except:
+            pass
+
     def execute(self, context):
         action_config = self.get_action_config()
-        
+
         param0 = self.get_param_var_name()
 
         filename = context.evaluate(param0)
@@ -75,3 +100,5 @@ class HttpPostFileAction(BaseAction):
             print(resp.text)
             return_var = self.get_return_var_name()
             context.set_context_var(return_var, value)
+            self.upload2newplatform(post_url, file)
+        return True
